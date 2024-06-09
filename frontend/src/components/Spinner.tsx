d258b3ff1a61d5e29715c73bb209d7885c1453d8
gilbertmunuotz <officialgilbert24@gmail.com>
2024-06-09 10:29:58 +0300

    started frontend

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,21 @@
+
+import BeatLoader from 'react-spinners/BeatLoader'
+import { SpinnerProps } from '../Types/FetchedNotes';
+
+function Spinner(props: SpinnerProps) {
+
+    const { loading } = props;
+
+    return (loading && (
+        <div className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-90 z-50">
+            <BeatLoader
+                loading={loading}
+                size={15}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+        </div>
+    ))
+}
+
+export default Spinner;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+import { FetchedNotes } from "../Types/FetchedNotes";
+
+function Home() {
+
+    const [notes, setNotes] = useState<FetchedNotes[]>([])
+    const [isloading, setIsLoading] = useState(true);
+    const url = `/api/allNotes`
+
+    useEffect(() => {
+        setTimeout(() => {
+            async function fetchData() {
+                setIsLoading(true)
+                try {
+                    const response = await fetch(url, {
+                        method: 'GET'
+                    });
+                    const data = await response.json()
+                    setNotes(data.notes);
+                } catch (error) {
+                    console.error('Error Fetching Data', error);
+                } finally {
+                    setIsLoading(false) // Set loading to false regardless of success or error
+                }
+            }
+            fetchData();
+        }, 4 * 1000);
+    }, [url])
+
+    return (
+        <div>
+            {isloading ? (
+                <Spinner loading={isloading} /> //Pass loading state to Spinner component
+            ) : (
+                notes.map(note => (
+                    <div key={note._id}>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                            <h1>{note.title}</h1>
+                            <p>{note.text}</p>
+                            
+                        </div>
+                    </div>
+                ))
+            )}
+        </div>
+    )
+}
+
+export default Home
